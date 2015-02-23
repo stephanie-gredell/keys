1ae888173b58d3b50884a0aa380cea02d194a230
Stephanie Chung <s.raide@gmail.com>
2015-02-22 23:05:26 -0800

    adding signup/login view

---
+++ js/views/LoginSignUpView.js
@@ -0,0 +1,12 @@
+var Backbone = require('backbone');
+var template = require('templates/loginsignup');
+var $ = require('jquery');
+require('foundation');
+
+module.exports = Backbone.View.extend({
+    initialize: function() {
+        $(document).foundation();
+        $(this.el).append(template());
+    },
+    el: 'body'
+});
