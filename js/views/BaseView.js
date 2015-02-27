8386264530f710f4dfe3f107ef0216fb899a5a73
Stephanie Chung <s.raide@gmail.com>
2015-02-27 00:26:31 -0800

    created BaseView

---
+++ js/views/BaseView.js
@@ -0,0 +1,11 @@
+var Backbone = require('backbone');
+
+module.exports = Backbone.View.extend({
+   render: function($element) {
+       $element.append(this.el);
+
+       if (this.afterRender) {
+           this.afterRender();
+       }
+   }
+});
