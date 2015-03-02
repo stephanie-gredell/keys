b37526b539c5d274993e53b23b84eed61408c2e2
Stephanie Chung <s.raide@gmail.com>
2015-03-01 22:22:46 -0800

    Initial commit for AppController.

---
+++ js/controllers/AppController.js
@@ -0,0 +1,22 @@
+var Fiber = require('fiber');
+var UserModel = require('models/UserModel');
+var LoginManager = require('managers/LoginManager');
+var $ = require('jquery');
+require('cookie');
+
+module.exports = Fiber.extend(function() {
+  return {
+    _userModel: new UserModel(),
+    /**
+     * On init, check if the user's cookie is set
+     * If it is, start the app else show loginView
+     */
+    init: function() {
+      if (!$.cookie('username')) {
+        LoginManager.showLoginView($('body'));
+      } else {
+        console.log(LoginManager);
+      }
+    }
+  };
+});
