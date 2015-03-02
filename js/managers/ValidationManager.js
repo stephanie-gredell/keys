a88e8c8f0a3f0d4aea75cfeddc39cd024b8c1223
Stephanie Chung <s.raide@gmail.com>
2015-03-01 22:22:58 -0800

    Commented validation manager

--- js/managers/ValidationManager.js
+++ js/managers/ValidationManager.js
@@ -3,6 +3,11 @@ var _ = require("underscore");
 
 var ValidationManager = Fiber.extend(function () {
   return {
+    /**
+     * validate a user's email to make sure it is formatted correctly
+     * @param {string} email a user's email address
+     * @returns {boolean}
+     */
     validateEmail: function (email) {
       var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(email);
