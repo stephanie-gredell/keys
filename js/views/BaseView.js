2bf7782f6f44b62b2f08c4870cb4c55f28265978
Stephanie Chung <s.raide@gmail.com>
2015-03-01 21:29:29 -0800

    adding additional check to BaseView

--- js/views/BaseView.js
+++ js/views/BaseView.js
@@ -2,7 +2,10 @@ var Backbone = require('backbone');
 
 module.exports = Backbone.View.extend({
   render: function ($element) {
-    $element.append(this.el);
+    if ($element) {
+      $element.append(this.el);
+    }
+
 
     if (this.afterRender) {
       this.afterRender();
