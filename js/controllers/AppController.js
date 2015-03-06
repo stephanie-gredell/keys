ca675e94a706654e7d9c7e0cffa1e75254087650
Stephanie Chung <s.raide@gmail.com>
2015-03-05 20:18:57 -0800

    removing login functionality on app init for now

--- js/controllers/AppController.js
+++ js/controllers/AppController.js
@@ -2,23 +2,14 @@ var Fiber = require('fiber');
 var UserModel = require('models/UserModel');
 var LoginManager = require('managers/LoginManager');
 var $ = require('jquery');
-var PianoManager = require('managers/PianoManager');
+var VideoManager = require('managers/VideoManager');
 var STRINGS = require('utils/strings');
 require('cookie');
 
 module.exports = Fiber.extend(function() {
   return {
-    _userModel: new UserModel(),
-    /**
-     * On init, check if the user's cookie is set
-     * If it is, start the app else show loginView
-     */
     init: function() {
-      if (!$.cookie(STRINGS.COOKIE.LOGIN.NAME)) {
-        LoginManager.showLoginView($('body'));
-      } else {
-        PianoManager.renderPianoView();
-      }
+      VideoManager.showVideo({videoUrl: '//player.vimeo.com/video/119618295'});
     }
   };
 });
